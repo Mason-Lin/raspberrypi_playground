9343c5e9e76b1613387be8298eaf739f00e9053a
Mason.Lin <pizza0117@gmail.com>
2020-05-27 00:01:13 +0800

    add cap single frame

--- camera/first_try/cv_capture_from_camera.py
+++ camera/first_try/cv_capture_from_camera.py
@@ -1,5 +1,12 @@
 import cv2
 cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+cv2.imwrite('ori.jpg', frame)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('gray.jpg', gray)
+
+
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
 fourcc = cv2.VideoWriter_fourcc('F', 'M', 'P', '4')
